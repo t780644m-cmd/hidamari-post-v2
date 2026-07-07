14072b06ecd3b8367417d9cf38dba514211cc294
t780644m-cmd <t780644m@kago.ed.jp>
2026-07-07 15:27:59 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const students = [
+  { id: 1, name: "あいき", icon: "🐱" },
+  { id: 2, name: "さくら", icon: "🌸" },
+  { id: 3, name: "ゆうと", icon: "🦖" },
+  { id: 4, name: "ひなた", icon: "☀️" },
+  { id: 5, name: "たくみ", icon: "⚽" },
+  { id: 6, name: "みお", icon: "🎀" },
+  { id: 7, name: "りく", icon: "🐻" },
+  { id: 8, name: "あおい", icon: "🐬" },
+  { id: 9, name: "こはる", icon: "🍀" }
+];
